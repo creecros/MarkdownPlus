b2d36f7a4563a9db49e5523565e82bff854fe6be
Tomas Dittmann <chaosmeist3r@gmail.com>
2023-02-16 08:44:41 +0100

    bring back checked checkboxes

--- vendor/leblanc-simon/parsedown-checkbox/ParsedownCheckbox.php
+++ vendor/leblanc-simon/parsedown-checkbox/ParsedownCheckbox.php
@@ -79,7 +79,7 @@ class ParsedownCheckbox extends ParsedownExtra
             $text = self::escape($text);
         }
 
-        return '<input type="checkbox" disabled /> ' . $this->format($text);
+        return '<input type="checkbox" checked disabled /> ' . $this->format($text);
     }
     
     /**
