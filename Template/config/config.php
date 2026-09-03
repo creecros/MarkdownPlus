31e9156c73697595aa07eaa69d72b69c7ba23244
redaxhu <redax@redax.hu>
2026-09-03 21:04:03 +0200

    Add config option for emoji word boundary matching

--- Template/config/config.php
+++ Template/config/config.php
@@ -2,3 +2,7 @@
     <?= $this->form->radio('unicode_shortcode', 'Oldschool Emojis' , 1, isset($values['unicode_shortcode'])&& $values['unicode_shortcode']==1) ?>
     <?= $this->form->radio('unicode_shortcode', 'Newschool Emojis ' , 2, isset($values['unicode_shortcode'])&& $values['unicode_shortcode']==2) ?>
 </div>
+<div class="panel">
+    <?= $this->form->radio('emoji_word_boundary', 'Replace :shortnames: anywhere' , 2, ! isset($values['emoji_word_boundary']) || $values['emoji_word_boundary']==2) ?>
+    <?= $this->form->radio('emoji_word_boundary', 'Replace :shortnames: only at word boundaries (keeps MAC addresses and file:line:col references intact)' , 1, isset($values['emoji_word_boundary']) && $values['emoji_word_boundary']==1) ?>
+</div>
